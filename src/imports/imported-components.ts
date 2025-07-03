7b65fe9b7ca3aa32ac5453e0b3d0f9403ccedc10
Chen Peleg <chenpeleg@gmail.com>
2025-07-03 09:38:58 +0300

    add TypeScript checks and improve documentation for run-on-file-change and run-on-startup scripts

--- src/imports/imported-components.ts
+++ src/imports/imported-components.ts
@@ -1,1 +1,8 @@
-
+/**
+ * This file is auto-generated. Do not edit manually.
+ * @readonly
+ * @module imported-components
+ */
+import '../components/big-card-component.ts';
+import '../components/card-component.ts';
+import '../undefined';
