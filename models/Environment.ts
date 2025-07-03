f843bf9a411a1d7140a9f8da3210cc9140252d7f
Chen Peleg <chenpeleg@gmail.com>
2025-07-03 10:13:45 +0300

    added services

--- models/Environment.ts
+++ models/Environment.ts
@@ -1,6 +1,8 @@
- export const Environment  = {
-     Development: 'development',
-     Production: 'production',
-     Test: 'test',
- } as const;
+export const Environment = {
+    Development: 'development',
+    Production: 'production',
+    Test: 'test',
+}
+
+export type Environment =   typeof  Environment[keyof typeof Environment];
 
