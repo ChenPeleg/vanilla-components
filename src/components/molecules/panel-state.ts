49ff7a2937e3c151deb5ca932946072b03592c46
Chen Peleg <chenpeleg@gmail.com>
2025-07-09 14:52:15 +0300

    very basic buttons work

--- src/components/molecules/panel-state.ts
+++ src/components/molecules/panel-state.ts
@@ -1,5 +1,6 @@
 import {ToggleButton} from '../atoms/toggle-button.ts';
 import {BaseElement} from '../../base/base-element.ts';
+import type {TextInput} from '../atoms/text-input.ts';
 
 class PanelState extends BaseElement {
     private state = {
@@ -17,28 +18,39 @@ class PanelState extends BaseElement {
             this.state.isActive = result.isActive;
             this.update();
         }
+        this.$<TextInput>('text-input').inputCallback = (result: { value: string }) => {
+            console.log('Input value:', result.value);
+            this.update();
+        };
 
     }
 
 
     render() {
         this.shadowRoot!.innerHTML = `  
-        <div class="flex flex-col items-center justify-center p-4 h-96 bg-amber-100">
-        <div class="flex flex-col items-center justify-center bg-amber-50/80   shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
-            <div id="active-state"> 
-            </div>
-            <toggle-button>   </toggle-button> <span class="text-blue-300">Toggle</span>
-        </div>
-        </div> 
-    `;
+            <div class="flex flex-col items-center justify-center p-4 h-96 bg-amber-100">
+                <div class="flex flex-col items-center justify-center bg-amber-50/80 shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
+                    <div>
+                        <span>
+                            State is <span id="active-state"></span>
+                        </span>
+                        <span>Panel State</span>
+                    </div>  
+                    <div>
+                        <text-input></text-input>
+                    </div>
+                    <toggle-button></toggle-button>
+                    <span class="text-blue-300">Toggle</span>
+                </div>
+            </div> 
+        `;
         this.update();
     }
 
     update() {
         const activeStateDiv = this.$('#active-state');
-        activeStateDiv.textContent = `This is ${this.state.isActive ? 'Active' : 'Not active'}`;
+        activeStateDiv.textContent = ` ${this.state.isActive ? 'Active' : 'Not active'}`;
     }
 }
 
 customElements.define('panel-state', PanelState);
-
--- src/imports/imported-components.ts
+++ src/imports/imported-components.ts
@@ -3,12 +3,13 @@
  * @readonly
  * @module imported-components
  */
-import '../components/atoms/card-component.ts';
-import '../components/molecules/logo-component.ts';
-import '../components/organism/nav-bar.ts';
-import '../components/organism/main-content.ts';
-import '../components/organism/root.ts';
-import '../components/atoms/app-button.ts';
-import '../components/molecules/panel-state.ts';
-import '../components/atoms/toggle-button.ts';
 import '../base/base-element.ts';
+import '../components/atoms/app-button.ts';
+import '../components/atoms/card-component.ts';
+import '../components/atoms/text-input.ts';
+import '../components/atoms/toggle-button.ts';
+import '../components/molecules/logo-component.ts';
+import '../components/molecules/panel-state.ts';
+import '../components/organism/main-content.ts';
+import '../components/organism/nav-bar.ts';
+import '../components/organism/root.ts';
