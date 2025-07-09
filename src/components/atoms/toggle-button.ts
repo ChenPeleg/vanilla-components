f2343be8777d16a4abe9bab574cd0e0bca08dddb
Chen Peleg <chenpeleg@gmail.com>
2025-07-09 09:49:07 +0300

    added toggle button

--- src/components/atoms/toggle-button.ts
+++ src/components/atoms/toggle-button.ts
@@ -23,23 +23,24 @@ class ToggleButton extends HTMLElement implements CustomElement {
 
         });
     }
+    clickHandler(_result: {isActive : boolean}): void {
+    };
     update() {
         const isActive = this.getAttribute('isActive') === 'true';
         const toggleButton = this.shadowRoot?.querySelector('button');
         if (toggleButton) {
-            const toggleCircle = toggleButton.querySelector('span');
+            const toggleCircle = toggleButton.querySelector('#toggle-nob');
             if (toggleCircle) {
                 toggleCircle.classList.toggle('translate-x-4', isActive);
             }
         }
-
     }
     render() {
         const _class = this.getAttribute('class') || '';
         const isActive = this.getAttribute('isActive') === 'true';
         (this.shadowRoot as ShadowRoot).innerHTML  = `   
         <button class="${_class} px-4 py-2 bg-blue-500 text-white rounded-full cursor-pointer hover:bg-blue-600 transition duration-200 flex items-center justify-center">
-            <span class="w-6 h-6 bg-white rounded-full ${isActive ? 'translate-x-4' : ''} transition-transform"></span>
+            <span id="toggle-nob" class="w-6 h-6 bg-white rounded-full ${isActive ? 'translate-x-4' : ''} transition-transform"></span>
             <slot></slot>
         </button> 
     `;
@@ -48,13 +49,6 @@ class ToggleButton extends HTMLElement implements CustomElement {
     attributeChangedCallback(_name: string, _oldValue: string, _newValue: string) {
         this.update();
     }
-
-
-
-    updateComponent() {
-        this.update();
-    }
-
 
 }
 
