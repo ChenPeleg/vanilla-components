2f438d0479382fd992346504395bd6a5c6f11910
Chen Peleg <chenpeleg@gmail.com>
2025-07-09 09:34:09 +0300

    added web components

--- src/components/atoms/toggle-button.ts
+++ src/components/atoms/toggle-button.ts
@@ -15,25 +15,34 @@ class ToggleButton extends HTMLElement implements CustomElement {
 
     connectedCallback(): void {
         (this.shadowRoot as ShadowRoot).adoptedStyleSheets = [globalStyleSheet];
-        this.render();
-    }
+        this.setInnerHTML();
 
-    attributeChangedCallback(_name: string, _oldValue: string, _newValue: string) {
-        this.render();
     }
-
-    render() {
+    setInnerHTML() {
         const _class = this.getAttribute('class') || '';
         const isActive = this.getAttribute('isActive') === 'true';
-        // make a tiggle button that toggles between two states (right and left) as a circle
-        this.shadowRoot!.innerHTML = `   
+        (this.shadowRoot as ShadowRoot).innerHTML  = `   
         <button class="${_class} px-4 py-2 bg-blue-500 text-white rounded-full cursor-pointer hover:bg-blue-600 transition duration-200 flex items-center justify-center">
             <span class="w-6 h-6 bg-white rounded-full ${isActive ? 'translate-x-4' : ''} transition-transform"></span>
             <slot></slot>
         </button> 
     `;
     }
+
+    attributeChangedCallback(_name: string, _oldValue: string, _newValue: string) {
+        this.update();
+    }
+
+
+
+    updateComponent() {
+        this.update();
+    }
+
+    update() {
+
+
+    }
 }
 
 customElements.define('toggle-button', ToggleButton);
-
