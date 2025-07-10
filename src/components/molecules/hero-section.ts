9938d4121ba7bb1cf1436bb8ef74278f7bd23396
Chen Peleg <chenpeleg@gmail.com>
2025-07-10 11:52:16 +0300

    custom elements

--- src/components/molecules/hero-section.ts
+++ src/components/molecules/hero-section.ts
@@ -19,7 +19,16 @@ class HeroSection extends BaseElement {
                 <logo-component></logo-component>
                 <div class="w-3/4 flex flex-row justify-center items-center text-gray-700 text-lg font-semibold">
                     Tools and practices to build a custom elements website in ${new Date().getFullYear()}
-                </div> 
+                </div>
+                <div class="flex flex-col">
+                    ${this.content.map(item => `
+                        <div class="flex flex-col  bg-white/10   shadow-lg rounded-lg p-6 m-4 w-full max-w-md">
+                            <h2 class="font-agbalumo text-2xl font-bold text-gray-800 mb-2">${item.header}</h2>
+                            <p class="text-gray-600">${item.text}</p>
+                        </div>
+                    `).join('')}
+                </div>
+                 
             </div>
         `;
     }
