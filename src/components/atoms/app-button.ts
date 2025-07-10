30d678ba5899b37a863e2e93b85734da0f6c787c
Chen Peleg <chenpeleg@gmail.com>
2025-07-10 14:33:03 +0300

    trying something

--- src/components/atoms/app-button.ts
+++ src/components/atoms/app-button.ts
@@ -5,6 +5,7 @@ export class AppButton extends BaseElement {
     static get observedAttributes() {
         return ['class', 'disabled'];
     }
+
     connectedCallback(): void {
         super.connectedCallback();
         this.$<HTMLButtonElement>('button').addEventListener('click', () => {
@@ -16,17 +17,17 @@ export class AppButton extends BaseElement {
         this.update();
     }
 
-    update () {
+    update() {
         const isDisabled = this.getAttribute('disabled') === 'true';
-        this.$<HTMLButtonElement>('button').disabled = isDisabled;
+        console.log(isDisabled);
+        this.$<HTMLButtonElement>('button').disabled    = isDisabled//= isDisabled;
         this.$<HTMLButtonElement>('button').setAttribute('aria-disabled', String(isDisabled));
     }
+
     renderTemplate() {
-        const _class = this.getAttribute('class') || '';
-        const isDisabled = this.getAttribute('disabled') === 'true';
         this.shadowRoot!.innerHTML = `   
-        <button disabled class="${_class} disabled:bg-red-300 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 transition duration-200" ${isDisabled ? 'disabled' : ''} aria-disabled="${isDisabled}">
-            <slot></slot>
+        <button class=" disabled:bg-blue-500/50 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 transition duration-200"   >
+           Click Me!
         </button>
     `;
     }
--- src/components/molecules/panel-state.ts
+++ src/components/molecules/panel-state.ts
@@ -1,22 +1,24 @@
 import {ToggleButton} from '../atoms/toggle-button.ts';
 import {BaseElement} from '../../_core/elements/base-element.ts';
 import {AppButton} from '../atoms/app-button.ts';
+
 // import type {TextInput} from '../atoms/text-input.ts';
 
 class PanelState extends BaseElement {
     private state = {
         isActive: true,
-        clicks : 0
+        clicks: 0
     }
+
     static get observedAttributes() {
         return ['header', 'text'];
     }
 
     connectedCallback() {
         super.connectedCallback();
-        this.$<ToggleButton>('toggle-button') .actionCallback = (result: { isActive: boolean }) => {
+        this.$<ToggleButton>('toggle-button').actionCallback = (result: { isActive: boolean }) => {
             this.state.isActive = result.isActive;
-            this.$<AppButton>('toggle-button').setAttribute('disabled', String(!this.state.isActive));
+            this.$<AppButton>('app-button').setAttribute('disabled', String(!this.state.isActive));
             this.update();
         };
         this.$<AppButton>('app-button').actionCallback = () => {
@@ -32,9 +34,9 @@ class PanelState extends BaseElement {
         this.shadowRoot!.innerHTML = `
             <div class="flex flex-col items-center justify-center p-4 h-96 bg-amber-100 ">
                 <div class="flex flex-col items-start justify-start bg-amber-50/80 shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
-                    <div class="flex flex-col items-center justify-center gap-2">
+                    <div class="flex flex-col items-start justify-start gap-2">
                         <span>
-                            State is <span id="active-state"></span>
+                            The Counter <span id="active-state"></span>
                         </span>
                         <span>
                            Count is <span id="count-text"></span>
@@ -42,7 +44,7 @@ class PanelState extends BaseElement {
                     </div>
                     <div>
                         <app-button>Click Me!</app-button>
-<!--                        <text-input></text-input>-->
+                        <!--                        <text-input></text-input>-->
                     </div>
                     <toggle-button defaultValue="${this.state.isActive.toString()}">
                         <span id="toggle-button-text" class="">Toggle</span>
@@ -55,10 +57,9 @@ class PanelState extends BaseElement {
     }
 
     update() {
-        this.$('#active-state').textContent = ` ${this.state.isActive ? 'Active' : 'Not active'}`;
-        this.$('#toggle-button-text').textContent = ` ${this.state.isActive ? 'Active' : 'Not active'}`
-        this.$('#count-text').textContent = ` ${this.state.clicks }`
-        // this.$('#text-from-input').textContent = (this.$<TextInput>('text-input')?.value || 'No input provided');
+        this.$('#active-state').textContent = `${this.state.isActive ? 'Active' : 'Not active'}`;
+        this.$('#toggle-button-text').textContent = `${this.state.isActive ? 'Active' : 'Not active'}`
+        this.$('#count-text').textContent = this.state.clicks.toString()
     }
 }
 
