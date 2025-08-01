7bf05aa58022f74f6b079cc7b39b2d92e1e4786c
Chen Peleg <chenpeleg@gmail.com>
2025-08-01 21:01:20 +0300

    services work

--- src/_core/router/router-outlet.ts
+++ src/_core/router/router-outlet.ts
@@ -5,12 +5,19 @@ import type {Subscription} from '../../models/Subscription.ts';
 
 
 export class RouterOutlet extends BaseElement {
-    private  readonly  servicesProvider = servicesProvider;
-    private subscription: Subscription;
+    private readonly servicesProvider = servicesProvider;
+    private subscription: Subscription | null = null;
 
     connectedCallback(): void {
         super.connectedCallback();
-        this.subscription = this.servicesProvider.getService(HashRouterService).sub
+        this.subscription = this.servicesProvider.getService(HashRouterService).subscribe((routerState => {
+            const outlet = this.$<HTMLDivElement>('#router-outlet');
+            if (!outlet) {
+                throw new Error('Router outlet not found');
+            }
+            const html = routerState.route?.element();
+            outlet.innerText = html || '';
+        }))
         this.update();
     }
 
@@ -20,7 +27,8 @@ export class RouterOutlet extends BaseElement {
     renderTemplate() {
         this.shadowRoot!.innerHTML = `<div class="contents" id="router-outlet"></div>`;
     }
-    disconnectedCallback(){
+
+    disconnectedCallback() {
         if (this.subscription) {
             this.subscription.unsubscribe();
         }
--- src/services/HashRouter.service.ts
+++ src/services/HashRouter.service.ts
@@ -1,5 +1,5 @@
-import { AbstractBaseService } from '../_global/provider/AbstractBaseService.ts';
-import { ServicesResolver } from '../_global/provider/ServiceResolverClass.ts';
+import {AbstractBaseService} from '../_global/provider/AbstractBaseService.ts';
+import {ServicesResolver} from '../_global/provider/ServiceResolverClass.ts';
 import {Router, type RouterState} from '../_core/router/router.ts';
 import {routes} from './routes.ts';
 
@@ -7,21 +7,27 @@ export class HashRouterService extends AbstractBaseService {
     private readonly router: Router
     private subscribers: { cb: (newState: RouterState) => void, id: number }[];
     private subscriberId = 0;
+
     constructor(provider: ServicesResolver) {
         super(provider);
         this.router = new Router({routes});
         this.subscribers = [];
     }
+
     getRouter(): Router {
         return this.router;
     }
+
     subscribe(fn: (newState: RouterState) => void) {
         this.subscribers.push({
             cb: fn,
             id: this.subscriberId
         });
-        return () => this.unsubscribe(this.subscriberId++);
+        return {
+            unsubscribe: () => this.unsubscribe(this.subscriberId++)
+        };
     }
+
     setState(routerState: RouterState) {
         this.subscribers.forEach(sub => sub.cb(routerState));
     }
