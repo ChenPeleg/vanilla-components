fbb6bbf71c9025c2c3b699609bf95f4c945ae898
Chen Peleg <chenpeleg@gmail.com>
2025-08-02 20:26:41 +0300

    added the hero section

--- src/components/organism/card-panel.ts
+++ src/components/organism/card-panel.ts
@@ -11,23 +11,7 @@ class CardPanel extends BaseElement {
     renderTemplate() {
         // language=HTML
         this.shadowRoot!.innerHTML = `
-            <div class="flex flex-col items-center justify-center p-4 h-96 bg-amber-100 ">
-                <div class="flex flex-col items-start justify-start bg-amber-50/80 shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
-                    <div class="flex flex-col items-start justify-start gap-2">
-                        <span>
-                            The Counter <span id="active-state"></span>
-                        </span>
-                        <span>
-                           Count is <span id="count-text"></span>
-                        </span>
-                    </div>
-                    <div>
-                        <app-button>Click Me!</app-button>
-                    </div>
-
-
-                </div>
-            </div>
+            <card-component header="Card Header" text="This is a card component"></card-component>
         `;
         this.update();
     }
