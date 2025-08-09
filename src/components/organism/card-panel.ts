d8e52fbaf4882dd563e23ddb4feb181f51d04e97
Chen Peleg <chenpeleg@gmail.com>
2025-08-09 22:04:46 +0300

    added overflow

--- src/components/organism/card-panel.ts
+++ src/components/organism/card-panel.ts
@@ -23,16 +23,32 @@ class CardPanel extends BaseElement {
         this.shadowRoot!.innerHTML = `
             <div class="flex flex-col items-center justify-center h-full gap-4 p-8">
                 <card-component id="card-when" header="When to use"  >
-                    <p class="text-gray-700 text-base">
+                    <p class="text-gray-700 text-base block overflow-ellipsis">
                         The most common use case for this when you have a site or some side project, that you want to showcase
                         in a single page. The advantage of this approach is that you have nearly zero dependencies to maintain.
                     </p>
                 </card-component>
                 <card-component id="card-how" header="How to use"  >
-                    <p class="text-gray-700 text-base">
+                    <article class="  text-gray-700 text-base block overflow-hidden overflow-ellipsis">
                         The basic use of html custom elements is to create a new element, that can be used in your HTML.
                         This get be tiresome. So this stack is actually a way to reduce the amount of boiler plate code you have to write.
-                    </p>
+                        <ul class="list-disc pl-5">
+                            <li>Define a custom element</li>
+                            <li>Use the custom element in your HTML</li>
+                            <li>Use the custom element in your JavaScript</li>
+                            <li>Use the custom element in your CSS</li>
+                        </ul>
+                        <p>
+                            The custom element can be used in your HTML like this:
+                        </p>
+                        <pre class="bg-gray-100 p-4 rounded">
+                            &lt;card-panel header="My Card Panel"&gt;
+                            &lt;/card-panel&gt;
+                        </pre>
+                        
+                    
+                    
+                    </article>
                 </card-component>
             </div>
         `;
@@ -45,8 +61,12 @@ class CardPanel extends BaseElement {
     }
 
     update() {
-        const card = this.$<HTMLElement>('#card') as HTMLElement;
-        card.setAttribute('height', this.state.openCard ? '420px' : '200px');
+        const cardHow = this.$<HTMLElement>('#card-how') as HTMLElement;
+        const cardWhen= this.$<HTMLElement>('#card-when') as HTMLElement;
+        cardHow.setAttribute('max-height', this.state.openCard  === 'how' ? '420px' : '200px');
+        cardHow.setAttribute('height', this.state.openCard  === 'how' ? '420px' : '200px');
+        cardWhen.setAttribute('max-height', this.state.openCard  === 'when' ? '420px' : '200px');
+        cardWhen.setAttribute('height', this.state.openCard  === 'when' ? '420px' : '200px');
     }
 }
 
