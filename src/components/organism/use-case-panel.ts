dd248de0913b4d5346f2fc3033aae60f145851c3
Chen Peleg <chenpeleg@gmail.com>
2025-08-09 22:19:42 +0300

    added overflow

--- src/components/organism/use-case-panel.ts
+++ src/components/organism/use-case-panel.ts
@@ -26,14 +26,21 @@ class UseCasePanel extends BaseElement {
                     <p class="text-gray-700 text-base block overflow-ellipsis">
                         The most common use case for this when you have a site or some side project, that you want to showcase
                         in a single page. The advantage of this approach is that you have nearly zero dependencies to maintain.
-                        in a single page. The advantage of this approach is that you have nearly zero dependencies to maintain.
                     </p>
                 </card-component>
                 <card-component id="card-how" header="How to use">
-                    <p class="text-gray-700 text-base text-ellipsis block overflow-hidden">
-                        The basic use of html custom elements is to create a new element, that can be used in your HTML.
-                        This get be tiresome. So this stack is actually a way to reduce the amount of boiler plate code you have to write.
-                    </p>
+                    <article>
+
+                        <p class="text-gray-700 text-base text-ellipsis block overflow-hidden">
+                            The basic use of html custom elements is to create a new element, that can be used in your HTML.
+                            This get be tiresome. So this stack is actually a way to reduce the amount of boiler plate code you have to
+                            write.
+                        </p>
+                        <p class="text-gray-700 text-base text-ellipsis block overflow-hidden">
+                          Check out:
+                            
+                        </p>
+                    </article>
                 </card-component>
             </div>
         `;
@@ -46,14 +53,14 @@ class UseCasePanel extends BaseElement {
     // }
 
     update() {
-        const cardHow = this.$<HTMLElement>('#card-how') as HTMLElement;
-        const cardWhen= this.$<HTMLElement>('#card-when') as HTMLElement;
-        cardHow.setAttribute('max-height', this.state.openCard  === 'how' ? '420px' : '200px');
-        cardHow.setAttribute('height', this.state.openCard  === 'how' ? '420px' : '200px');
-        cardHow.classList.toggle('line-clamp-3', this.state.openCard !== 'how');
-        cardWhen.setAttribute('max-height', this.state.openCard  === 'when' ? '420px' : '200px');
-        cardWhen.setAttribute('height', this.state.openCard  === 'when' ? '420px' : '200px');
-        cardWhen.classList.toggle('line-clamp-3', this.state.openCard !== 'when');
+        // const cardHow = this.$<HTMLElement>('#card-how') as HTMLElement;
+        // const cardWhen = this.$<HTMLElement>('#card-when') as HTMLElement;
+        // cardHow.setAttribute('max-height', this.state.openCard === 'how' ? '420px' : '200px');
+        // cardHow.setAttribute('height', this.state.openCard === 'how' ? '420px' : '200px');
+        // cardHow.classList.toggle('line-clamp-3', this.state.openCard !== 'how');
+        // cardWhen.setAttribute('max-height', this.state.openCard === 'when' ? '420px' : '200px');
+        // cardWhen.setAttribute('height', this.state.openCard === 'when' ? '420px' : '200px');
+        // cardWhen.classList.toggle('line-clamp-3', this.state.openCard !== 'when');
     }
 }
 
