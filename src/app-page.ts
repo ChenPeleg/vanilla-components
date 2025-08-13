df9d297d3cce0224ae7044960b86897771a1f959
Chen Peleg <chenpeleg@gmail.com>
2025-08-13 22:44:24 +0300

    hacked router

--- src/app-page.ts
+++ src/app-page.ts
@@ -1,24 +1,35 @@
 import {BaseElement} from './_core/elements/base-element.ts';
-import type {ToggleButton} from './site/components/atoms/toggle-button.ts';
-import type {AppButton} from './site/components/atoms/app-button.ts';
+
+
+export class SimpleButton extends BaseElement {
+    connectedCallback(): void {
+        super.connectedCallback();
+        this.$<HTMLButtonElement>('button').addEventListener('click', () => {
+            this.actionCallback({clicked: true});
+        });
+    }
+
+    renderTemplate() {
+        // language=HTML
+        this.shadowRoot!.innerHTML = `
+            <button class=" disabled:bg-blue-500/50 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 transition duration-200">
+                <slot></slot>
+            </button>
+        `;
+    }
+}
+customElements.define('simple-button', SimpleButton);
 
 
 class AppPage extends BaseElement {
     private state = {
-        isActive: true,
         clicks: 0
     }
+
     connectedCallback() {
         super.connectedCallback();
-        this.$<ToggleButton>('toggle-button').actionCallback = (result: { isActive: boolean }) => {
-            this.state.isActive = result.isActive;
-            this.$<AppButton>('app-button').setAttribute('disabled', String(!this.state.isActive));
-            this.update();
-        };
-        this.$<AppButton>('app-button').actionCallback = () => {
-            if (this.state.isActive) {
-                this.state.clicks++;
-            }
+        this.$<SimpleButton>('simple-button').actionCallback = () => {
+            this.state.clicks++;
             this.update();
         }
     }
@@ -26,34 +37,24 @@ class AppPage extends BaseElement {
     renderTemplate() {
         // language=HTML
         this.shadowRoot!.innerHTML = `
-            <div class="flex flex-col items-center justify-center p-4 h-96 bg-amber-100 ">
+            <div class="flex flex-col items-center justify-center p-4 h-96  ">
                 <div class="flex flex-col items-start justify-start bg-amber-50/80 shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
-                    <div class="flex flex-col items-start justify-start gap-2">
-                        <span>
-                            The Counter <span id="active-state"></span>
-                        </span>
-                        <span>
-                           Count is <span id="count-text"></span>
-                        </span>
-                    </div>
                     <div>
-                        <app-button>Click Me!</app-button> 
+                        <simple-button>
+                            Count is <span id="count-text"> 0 </span>
+                        </simple-button>
                     </div>
-                    <toggle-button defaultValue="${this.state.isActive.toString()}">
-                        <span id="toggle-button-text" class="">Toggle</span>
-                    </toggle-button>
-
                 </div>
             </div>
         `;
-        this.update();
     }
 
     update() {
-        this.$('#active-state').textContent = `${this.state.isActive ? 'Active' : 'Not active'}`;
-        this.$('#toggle-button-text').textContent = `${this.state.isActive ? 'Active' : 'Not active'}`
         this.$('#count-text').textContent = this.state.clicks.toString()
     }
 }
 
 customElements.define('app-page', AppPage);
+
+
+
