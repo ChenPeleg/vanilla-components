160970e40928c9b8c5bc9d79a1ea117dd5555585
Chen Peleg <chenpeleg@gmail.com>
2025-08-13 22:09:25 +0300

    added vanilla elements

--- src/root.ts
+++ src/root.ts
@@ -1,16 +1,14 @@
-
 import {BaseElement} from './_core/elements/base-element.ts';
-import {appConfig} from './configuration/appConfig.ts';
 
-class AppRoot extends BaseElement   {
+class AppRoot extends BaseElement {
 
     renderTemplate() {
-        console.log(appConfig.environment);
-        this.shadowRoot!.innerHTML = `  
-      <div class="h-screen w-screen overflow-hidden ">  
-         <router-outlet></router-outlet> 
-      </div>
-    `;
+        // language=HTML
+        this.shadowRoot!.innerHTML = `
+            <div class="h-screen w-screen overflow-hidden ">
+                <router-outlet></router-outlet>
+            </div>
+        `;
     }
 }
 
