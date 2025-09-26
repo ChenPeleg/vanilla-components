45edda0e3421062eb6712c2f1b2ff49b27466fc9
Chen Peleg <chenpeleg@gmail.com>
2025-09-26 17:11:56 +0300

    added html test app

--- tests/unit/app-button.spec.ts
+++ tests/unit/app-button.spec.ts
@@ -2,13 +2,11 @@ import { test, expect } from '@playwright/test';
 import {setPageHtml} from '../_tools/setPageHtml';
 
 test('has title 2', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+
     await page.goto('/')
-    await setPageHtml (page, ``);
+    await setPageHtml (page, `<app-button id="my-app-button"></app-button>`);
     const header = await page.getByRole('heading').innerText();
     expect(header).toBe('Smoke test page');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
 });
 
