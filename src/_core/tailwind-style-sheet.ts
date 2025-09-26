44a159aee142cbde4731494b88fc227b1e7a8492
Chen Peleg <chenpeleg@gmail.com>
2025-09-26 08:35:42 +0300

    another enum alternative

--- src/_core/tailwind-style-sheet.ts
+++
@@ -1,6 +0,0 @@
-
-import markdownString from '../style.css?inline'
-
-const cssStyleSheet = new CSSStyleSheet();
-cssStyleSheet.replaceSync(markdownString);
-export const globalStyleSheet = cssStyleSheet;
--- src/models/AppAction.ts
+++
@@ -1,5 +0,0 @@
-import {AppActionType} from '../store/app-action-type.ts';
-
-export type AppAction =
-    |   { type: AppActionType ; payload: any; }
-
