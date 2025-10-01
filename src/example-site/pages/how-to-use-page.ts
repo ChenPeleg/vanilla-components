54611eb445143bdc8601723295ee931f4fab9c41
Chen Peleg <chenpeleg@gmail.com>
2025-10-01 19:26:31 +0300

    fixed router problem

--- src/example-site/pages/how-to-use-page.ts
+++ src/example-site/pages/how-to-use-page.ts
@@ -1,6 +1,28 @@
 import {BaseElement} from '../../_core/elements/base-element.ts';
+import {documentation, DocumentationKind, type DocumentationType} from '../documentation/documentation.ts';
 
 class HowToUsePage extends BaseElement {
+    private howTouseDocs = documentation
+    constructor() {
+        super();
+
+    }
+    buildDocUnit(doc: DocumentationType) {
+        switch (doc.type) {
+            case DocumentationKind.header:
+                return `<header-2> ${doc.content} </header-2>`;
+            case DocumentationKind.code:
+                return `<code-gist> ${doc.content} </code-gist>`;
+            case DocumentationKind.html:
+                return doc.content;
+            case DocumentationKind.text:
+            default:
+                return `<p class="text-gray-700 text-lg mt-4"> ${doc.content} </p>`;
+
+
+        }
+
+    }
 
     renderTemplate() {
         // language=HTML
@@ -8,13 +30,7 @@ class HowToUsePage extends BaseElement {
             <main class="overflow-hidden bg-amber-200  w-full h-full p-5 flex flex-col items-center">
                 <article class="max-w-3xl">
                     <header-1> How to use  </header-1>
-                    <header-2>  The base element </header-2>
-                    <p class="text-gray-700 text-lg mt-4">
-                        The base element is a custom web component that extends the functionality of the native HTML element.
-                        It provides a foundation for creating reusable components with encapsulated styles and behavior.
-                    </p>
-                    <code-gist> 
-                    </code-gist>
+                     ${this.howTouseDocs.map(doc => this.buildDocUnit(doc)).join('')}
                 </article>
             </main>
         `;
