de36019ed791bd690baa1e957e68860ecfc4a093
Chen Peleg <chenpeleg@gmail.com>
2025-10-01 15:07:42 +0300

    Add store subscription to GlobalCounter for state updates

--- src/example-site/components/atoms/global-counter.ts
+++ src/example-site/components/atoms/global-counter.ts
@@ -1,7 +1,11 @@
 import {BaseElement} from '../../../_core/elements/base-element.ts';
+import {StoreService} from '../../../services/Store.service.ts';
+import type {Subscription} from '../../../models/Subscription.ts';
 
 
 export class GlobalCounter extends BaseElement {
+    private storeSubscription: Subscription | undefined = undefined;
+
     static get observedAttributes() {
         return ['class', 'disabled'];
     }
@@ -9,24 +13,34 @@ export class GlobalCounter extends BaseElement {
     connectedCallback(): void {
 
         super.connectedCallback();
+        this.storeSubscription = this.servicesProvider.getService(StoreService).subscribe(() => {
+            this.update()
+        })
 
         this.update();
     }
 
     update() {
-        const isDisabled = this.getAttribute('disabled') === 'true';
-
-        this.setAttribute('disabled', isDisabled ? 'true'  : 'false') ;
-        this.setAttribute('disabled', isDisabled ? 'true'  : 'false') ;
-        this.setAttribute('aria-disabled', String(isDisabled));
+        const state = this.servicesProvider.getService(StoreService).store.getState();
+        const span = this.$<HTMLSpanElement>('#global-count');
+        if (span) {
+            span.textContent = state.count.toString();
+        }
     }
 
     renderTemplate() {
         this.shadowRoot!.innerHTML = `   
         <div class=" disabled:bg-blue-500/50 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 transition duration-200"   >
-           Click Me!
+          Global Count: <span id="global-count">0</span>
         </div>
     `;
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        if (this.storeSubscription) {
+            this.storeSubscription.unsubscribe()
+        }
     }
 }
 
