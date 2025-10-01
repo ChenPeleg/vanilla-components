faf164c006b95e9725a4d326d0cbe50734f587ef
Chen Peleg <chenpeleg@gmail.com>
2025-10-01 19:02:42 +0300

    fixed router problem

--- src/example-site/components/molecules/code-gist.ts
+++ src/example-site/components/molecules/code-gist.ts
@@ -1,9 +1,7 @@
 import { BaseElement } from '../../../_core/elements/base-element.ts';
 
 class CodeGist extends BaseElement {
-    static get observedAttributes() {
-        return ['width'];
-    }
+    static get observedAttributes() { return ['width']; }
 
     connectedCallback(): void {
         super.connectedCallback();
@@ -12,22 +10,16 @@ class CodeGist extends BaseElement {
     }
 
     renderTemplate() {
-        // Use an iframe with srcdoc to embed the gist
-        const gistId = '6bf78a90f12203ed2228f5f214e75f61';
-        const gistUser = 'ChenPeleg';
-        const srcdoc = `\n<!DOCTYPE html>\n<html>\n<head>\n  <base target='_parent'>\n</head>\n<body>\n  <script src='https://gist.github.com/${gistUser}/${gistId}.js'></script>\n</body>\n</html>\n`;
+        const srcdoc = `<!DOCTYPE html><html><head><base target='_parent'></head><body><script src='https://gist.github.com/ChenPeleg/6bf78a90f12203ed2228f5f214e75f61.js'></script></body></html>`;
         this.shadowRoot!.innerHTML = `
             <div class="flex flex-col items-center justify-center p-4">
                 <iframe
                     class="w-full border rounded"
                     style="min-height:300px; background:white;"
                     width="100%"
-                    height="400"
-                    srcdoc='${srcdoc.replace(/'/g, "&#39;")}'
-                    title="GitHub Gist"
-                    frameborder="0"
-                    loading="lazy"
-                ></iframe>
+                    height="100%"
+                    srcdoc='${srcdoc.replace(/'/g, "&#39;")}'>
+                </iframe>
             </div>
         `;
     }
