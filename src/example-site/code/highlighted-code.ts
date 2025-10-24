fb57684f7e35077e5ba1a81cd70493f2520fe0d8
Chen Peleg <chenpeleg@gmail.com>
2025-10-24 16:58:07 +0300

    feat: add highlightedCode type and implement highlighted code component

--- src/example-site/code/highlighted-code.ts
+++ src/example-site/code/highlighted-code.ts
@@ -1,7 +1,9 @@
 import {BaseElement} from '../../_core/elements/base-element.ts';
+import {SyntaxtHighlihter} from './syntaxt-highlihter.ts';
 
 
 export class HighlightedCode extends BaseElement {
+    syntaxtHighlighter = new SyntaxtHighlihter()
 
 
     static get observedAttributes() {
@@ -19,7 +21,8 @@ export class HighlightedCode extends BaseElement {
 
     legacyRender() {
         const rawCode = this.getAttribute('code') || '';
-        return `<pre class="bg-gray-800 text-white p-2 sm:p-4 rounded-lg overflow-x-auto text-xs sm:text-sm break-words whitespace-pre-wrap"><code class="break-words">${rawCode}</code></pre>`;
+        const styledCode = this.syntaxtHighlighter.applyStylesToHighlightedCode (rawCode)
+        return `<pre class="bg-gray-800 text-white p-2 sm:p-4 rounded-lg overflow-x-auto text-xs sm:text-sm break-words whitespace-pre-wrap"><code class="break-words">${styledCode}</code></pre>`;
 
     }
 
