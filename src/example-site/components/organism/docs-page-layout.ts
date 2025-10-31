fc51dd52d184577c280062abdea4bbfc33f38651
Chen Peleg <chenpeleg@gmail.com>
2025-10-31 11:07:32 +0200

    docs: update documentation rendering method and import new components

--- src/example-site/components/organism/docs-page-layout.ts
+++ src/example-site/components/organism/docs-page-layout.ts
@@ -17,7 +17,7 @@ export class DocsPageLayout extends BaseElement {
         const intro = this.getAttribute('intro') || '';
 
         this.shadowRoot!.innerHTML = `
-            <main class="overflow-y-auto ${bg} w-full h-full px-4 md:px-5 lg:px-8 py-5 flex flex-col items-center">
+            <main class="overflow-y-auto ${bg} w-full h-full px-4 md:px-5   lg:px-8 py-5 flex flex-col items-center">
                 <article class="max-w-full md:max-w-3xl">
                     <header-1> ${title} </header-1>
                     ${intro ? `<p class=\"text-gray-700 text-base sm:text-lg mt-4\">${intro}</p>` : ''}
