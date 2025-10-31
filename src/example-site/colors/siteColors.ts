cd41015a92ff2b4e0966f99b6350328b46aa9a24
Chen Peleg <chenpeleg@gmail.com>
2025-10-31 13:18:50 +0200

    docs: adjust examples background color for improved visibility

--- src/example-site/colors/siteColors.ts
+++ src/example-site/colors/siteColors.ts
@@ -4,7 +4,7 @@ export class SiteColors {
    static textMain = 'text-gray-700 dark:text-gray-300';
    static howToUseBgColor = 'bg-amber-200 dark:bg-blue-900/50';
    static mainVanillaBgColor = 'bg-green-400 dark:bg-green-700/50';
-   static examplesBgColor = 'bg-amber-100 dark:bg-red-600/15';
+   static examplesBgColor = 'bg-amber-100 dark:bg-red-600/20';
    static tutorialFoldersBg = 'bg-blue-50 dark:bg-blue-900/50';
    static cardBackgroundBg = 'bg-white dark:bg-slate-900';
 }
