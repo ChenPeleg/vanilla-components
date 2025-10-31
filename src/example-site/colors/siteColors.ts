3000fce2268315db487253114b9d0a713059b927
Chen Peleg <chenpeleg@gmail.com>
2025-10-31 13:15:01 +0200

    docs: improve spacing and structure in documentation rendering components

--- src/example-site/colors/siteColors.ts
+++ src/example-site/colors/siteColors.ts
@@ -2,10 +2,9 @@ export class SiteColors {
     static headerText = "text-gray-800 dark:text-gray-100";
 
    static textMain = 'text-gray-700 dark:text-gray-300';
-   static howToUseBgColor = 'bg-amber-200 dark:bg-amber-900/50';
+   static howToUseBgColor = 'bg-amber-200 dark:bg-blue-900/50';
    static mainVanillaBgColor = 'bg-green-400 dark:bg-green-700/50';
-   static stateManagementBgColor = 'bg-amber-100 dark:bg-amber-900/30';
-   static examplesBgColor = 'bg-fuchsia-100 dark:bg-fuchsia-900/50';
+   static examplesBgColor = 'bg-amber-100 dark:bg-stone-900/90';
    static tutorialFoldersBg = 'bg-blue-50 dark:bg-blue-900/50';
    static cardBackgroundBg = 'bg-white dark:bg-slate-900';
 }
--- src/example-site/pages/home-page.ts
+++ src/example-site/pages/home-page.ts
@@ -7,7 +7,7 @@ class HomePage extends BaseElement {
     renderTemplate() {
         // language=HTML
         this.shadowRoot!.innerHTML = `
-            <main class="overflow-y-auto    w-full h-full">
+            <main class="overflow-y-auto w-full h-full">
                 <div class="flex flex-col lg:flex-row justify-center lg:h-full">
                     <div class="flex-1 ${SiteColors.howToUseBgColor} flex flex-col items-center justify-start">
                         <use-case-panel></use-case-panel>
@@ -15,7 +15,7 @@ class HomePage extends BaseElement {
                     <div class="flex-1  ${SiteColors.mainVanillaBgColor} flex flex-col items-center justify-start">
                         <hero-section></hero-section>
                     </div>
-                    <div class="flex-1 ${SiteColors.stateManagementBgColor} ">
+                    <div class="flex-1 ${SiteColors.examplesBgColor} ">
                         <state-management-panel></state-management-panel>
                     </div>
                 </div>
