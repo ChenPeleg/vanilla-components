95d37023d2d23cbea96b1f45a638776759b457c9
Chen Peleg <chenpeleg@gmail.com>
2025-11-14 11:18:30 +0200

    feat: implement action callback for list items in ExamplesListPanel and enhance ListItem component

--- src/example-site/components/molecules/list-item.ts
+++ src/example-site/components/molecules/list-item.ts
@@ -1,24 +1,46 @@
-import { BaseElement } from '../../../_core/elements/base-element.ts';
+import {BaseElement} from '../../../_core/elements/base-element.ts';
 import {SiteColors} from '../../colors/siteColors.ts';
 
 export class ListItem extends BaseElement {
-    static get observedAttributes() { return ['text', 'id' ]; }
+    static get observedAttributes() {
+        return ['text', 'id'];
+    }
 
     connectedCallback(): void {
         super.connectedCallback();
         this.renderTemplate();
     }
 
+    actionCallback = ({
+                          id,
+                          actionType
+                      }: {
+        id: string;
+        actionType: string;
+    }) => {
+
+    }
+
     renderTemplate() {
         const text = this.getAttribute('text') || '';
-         this.shadowRoot!.innerHTML = `
+        // language=HTML
+        this.shadowRoot!.innerHTML = `
             <div class="flex w-full  flex-row items-center justify-center px-4">
-                  <div class=" w-full flex flex-col items-start justify-start ${SiteColors.cardBackgroundBg} shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
-                    <div class="flex flex-col items-start justify-start gap-2">
-                        <span >
+                <div class=" w-full flex flex-col items-start justify-start ${SiteColors.cardBackgroundBg} shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
+                    <div class="flex flex-row items-center justify-between gap-2 w-full">
+                        <span>
                             ${text}
-                        </span> 
-                    </div> 
+                        </span>
+                        <div>   
+                            <span>
+                            <button id="trash-button"
+                                    class="h-7 w-7 rounded-full cursor-pointer text-white hover:bg-slate-400 transition"> 
+                                🗑️ 
+                            </button>
+                            </span>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         `;
