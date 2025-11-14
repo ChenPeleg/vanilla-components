7f8ad48af0c959f0460548cf4d3848855c2ffcf5
Chen Peleg <chenpeleg@gmail.com>
2025-11-14 10:46:35 +0200

    fix: correct typo in ListItem class name and clean up renderTemplate method

--- src/example-site/components/molecules/list-item.ts
+++ src/example-site/components/molecules/list-item.ts
@@ -1,28 +1,20 @@
 import { BaseElement } from '../../../_core/elements/base-element.ts';
 
-class ListItme extends BaseElement {
+class ListItem extends BaseElement {
     static get observedAttributes() { return ['text', 'id' ]; }
 
     connectedCallback(): void {
         super.connectedCallback();
-        this.setAttribute('role', 'region');
         this.renderTemplate();
     }
 
     renderTemplate() {
-        const srcdoc = `<!DOCTYPE html><html><head><base target='_parent'></head><body><script src='${this.getAttribute('gist')}'></script></body></html>`;
-        this.shadowRoot!.innerHTML = `
+         this.shadowRoot!.innerHTML = `
             <div class="flex flex-col items-center justify-center p-4">
-                <iframe
-                    class="w-full border rounded"
-                    style="min-height:300px; background:white;"
-                    width="100%"
-                    height="100%"
-                    srcdoc='${srcdoc.replace(/'/g, "&#39;")}'>
-                </iframe>
+               
             </div>
         `;
     }
 }
 
-customElements.define('list-item', ListItme);
+customElements.define('list-item', ListItem);
