1c0aaf41fa0d731d9d18c3044f235efa047a6a95
Chen Peleg <chenpeleg@gmail.com>
2025-11-14 10:57:21 +0200

    feat: update ExamplesListPanel and ListItem components to render dynamic text

--- src/example-site/components/molecules/list-item.ts
+++ src/example-site/components/molecules/list-item.ts
@@ -10,19 +10,15 @@ class ListItem extends BaseElement {
     }
 
     renderTemplate() {
+        const text = this.getAttribute('text') || '';
          this.shadowRoot!.innerHTML = `
             <div class="flex w-full  flex-row items-center justify-center p-4">
                   <div class=" w-full flex flex-col items-start justify-start ${SiteColors.cardBackgroundBg} shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
                     <div class="flex flex-col items-start justify-start gap-2">
-                        <span>
-                            The Counter <span id="active-state"></span>
-                        </span>
-                        <span>
-                           Count is <span id="count-text"></span>
-                        </span>
-                    </div>
-                  
-
+                        <span >
+                            ${text}
+                        </span> 
+                    </div> 
                 </div>
             </div>
         `;
--- src/example-site/components/organism/examples-list-panel.ts
+++ src/example-site/components/organism/examples-list-panel.ts
@@ -3,16 +3,19 @@ import {BaseElement} from '../../../_core/elements/base-element.ts';
 
 class ExamplesListPanel extends BaseElement {
 
+    state = {
+        items: ['buy milk', 'walk the dog', 'do the laundry']
+    }
+
     connectedCallback() {
         super.connectedCallback();
-
     }
 
     renderTemplate() {
         // language=HTML
         this.shadowRoot!.innerHTML = `
             <div class="  w-full flex flex-row items-center justify-center p-4 h-96  ">
-                <list-item></list-item>
+                <list-item  text="${'but milk'}"></list-item>
             </div>
         `;
         this.update();
