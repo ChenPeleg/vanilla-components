080961dd7ea2b83e7e45f5ca69c2970bbb673c95
Chen Peleg <chenpeleg@gmail.com>
2025-11-14 10:48:05 +0200

    feat: enhance ListItem component with improved layout and styling

--- src/example-site/components/molecules/list-item.ts
+++ src/example-site/components/molecules/list-item.ts
@@ -1,4 +1,5 @@
 import { BaseElement } from '../../../_core/elements/base-element.ts';
+import {SiteColors} from '../../colors/siteColors.ts';
 
 class ListItem extends BaseElement {
     static get observedAttributes() { return ['text', 'id' ]; }
@@ -10,8 +11,19 @@ class ListItem extends BaseElement {
 
     renderTemplate() {
          this.shadowRoot!.innerHTML = `
-            <div class="flex flex-col items-center justify-center p-4">
-               
+            <div class="flex w-full  flex-row items-center justify-center p-4">
+                  <div class="flex flex-col items-start justify-start ${SiteColors.cardBackgroundBg} shadow-lg rounded-lg p-6 w-full max-w-md gap-6">
+                    <div class="flex flex-col items-start justify-start gap-2">
+                        <span>
+                            The Counter <span id="active-state"></span>
+                        </span>
+                        <span>
+                           Count is <span id="count-text"></span>
+                        </span>
+                    </div>
+                  
+
+                </div>
             </div>
         `;
     }
